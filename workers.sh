8d9ef398ce6e3865d2e170b828913eb725d58e68
Ducca <64695363+duccas@users.noreply.github.com>
2023-10-17 15:25:26 +0300

    fix

--- workers.sh
+++ workers.sh
@@ -7,8 +7,7 @@ tmux new-session -d -s snark-workers
 tmux send-keys -t snark-workers:0 "RAYON_NUM_THREADS=${RAYON_NUM_THREADS_WORKER} mina internal snark-worker --daemon-address ${COORDINATOR_IP}:8301 --proof-level full --shutdown-on-disconnect false" C-m
 
 tmux new-window -t snark-workers
-tmux new-window -t snark-workers
-tmux new-window -t snark-workers
+tmux new-window -t snark-workers    
 
 tmux send-keys -t snark-workers:1 "RAYON_NUM_THREADS=${RAYON_NUM_THREADS_WORKER} mina internal snark-worker --daemon-address ${COORDINATOR_IP}:8301 --proof-level full --shutdown-on-disconnect false" C-m
 tmux send-keys -t snark-workers:2 "RAYON_NUM_THREADS=${RAYON_NUM_THREADS_WORKER} mina internal snark-worker --daemon-address ${COORDINATOR_IP}:8301 --proof-level full --shutdown-on-disconnect false" C-m
